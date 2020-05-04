3e0228ce6c5008e640767ae372b7ba1083801529
WCCharles <C.walker10@edu.salford.ac.uk>
2020-05-04 13:58:42 +0100

    additions to setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,18 @@ setuptools.setup(name='alohaecc',
                  version='0.0.1',
                  author='Christopher Walker',
                  author_email='chris@elk.audio',
-                 description="A prototypical Error Concealment \
-                     bench test for Aloha",
+                 description="A prototypical Error Concealment
+                              bench test for Aloha",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
+                 url="https://bitbucket.org/mindswteam/alohaecc"
                  packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                 ]
                  install_requires=[
                     'numpy',
                     'soundfile',
                  ],
+                 python_requires='>=3.7',
                  )
