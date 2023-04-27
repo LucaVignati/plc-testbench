d0014106b516e4d9f473d96eb588f7bff74a756b
Stefano Dallona <stefano.dallona@electrolux.com>
2023-04-27 20:21:39 +0200

    Testing commit on branch

--- plctestbench/worker.py
+++ plctestbench/worker.py
@@ -6,3 +6,13 @@ class Worker(object):
 
     def run(self) -> None:
         pass
+
+class OriginalTrackWorker(Worker):
+    def __init__(self, settings: GlobalSettings) -> None:
+        super().__init__(settings)
+
+    def run(self, uuid: str) -> None:
+        self.uuid = uuid
+        for idx in self.settings.__progress_monitor__(self)(range(0, 10), desc=self.__str__(), mininterval=0.1, maxinterval=0.1):
+            print("Worker %s (%s)" % (self.uuid, self.__class__.__name__))
+            #sleep(1)
